fc9d8b5d3f4ef9e58e0d00eebb7371f3dc0d0fb9
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-02-20 19:55:42 +0100

    ft: create navbar and hero-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'darkblue': '#0B0A1B',
+      'black': '#000000',
+      'nav_border': '#E9ECF0',
+      'grey': '#8C98AD',
+      'lightblue' : '#91A2CB'
+      
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],

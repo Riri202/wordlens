f1b4e98a30512d114d9003fb7dafb9a61143a00c
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-02-22 21:16:10 +0100

    chore: api consumption

--- src/index.js
+++ src/index.js
@@ -2,38 +2,51 @@
 var searchBtn = document.getElementById("search");
 var infoSkeleton = document.getElementById("infoSkeleton")
 var infoWord = document.getElementById("infoWord")
+var definition = document.getElementById("definition")
+var sentence = document.getElementById("sentence")
+var synonym = document.getElementById("synonym")
+
 
 
 
             // Functions
- //API consumption
+    //API consumption
  async function fetchApi(word) {
      infoSkeleton.classList.remove('hidden')
     try {
-        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`; 
         const res = await fetch(url);
         const data = await res.json();
-        infoSkeleton.classList.add('hidden')       
-      
-       displayData(data, word);
-       
+        infoSkeleton.classList.add('hidden')               
+            displayData(data, word);
+           
         
     } catch (error) {
         console.log(error);
     }}
 
 //display data on page
- const displayData = (data) => {
-    console.log(data)
- }   
+ function displayData(data, word) {
+    console.log(data);
+    infoWord.innerText = `${word}`;
+     //display meanings
+    // const meanings = data[0].meanings[0].definitions;
+    // let meaningList = ``;
+    // meanings.forEach((meaning, ind) => {
+    //   meaningList += `${ind + 1}. ${meaning.definition} \n`;
+    //   definition.innerText = meaningList
+    //   });
+ 
+ } 
 
-
-
-             // Event Listeners
-    input.addEventListener('keyup', e => {
-        if (e.key === 'enter' && e.target.value) {
-            fetchApi(e.target.value);
+           // Event Listeners
+    input.addEventListener('keyup', async (e) => {
+        console.log(e.key, e.target.value)
+        if (e.keyCode === 13 && e.target.value) {
+            e.preventDefault();
+            await fetchApi(e.target.value);
         }
     });
-    searchBtn.addEventListener('click', fetchApi());
+   
+    //searchBtn.addEventListener('click', fetchApi(word));
 

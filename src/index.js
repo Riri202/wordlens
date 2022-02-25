0048fa578504305b68b68ef84248d5bcdd847892
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-02-25 21:01:03 +0100

    ft: implement api consumption to display definitions, synonyms and examples

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var searchBtn = document.getElementById("search");
 var infoSkeleton = document.getElementById("infoSkeleton")
 var infoWord = document.getElementById("infoWord")
 var definition = document.getElementById("definition")
-var example = document.getElementById("example")
+ var example = document.getElementById("example")
 var synonym = document.getElementById("synonym")
 var dataDisplay = document.getElementById("data_display")
 var phoneticPara = document.getElementById("phoneticPara")
@@ -22,6 +22,7 @@ var phoneticPara = document.getElementById("phoneticPara")
 async function fetchApi(word) {
     infoSkeleton.classList.remove('hidden')
     dataDisplay.classList.add('hidden')
+    error.classList.add('hidden')
    try {
        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`; 
        const res = await fetch(url);
@@ -41,40 +42,80 @@ toggleBtn.addEventListener('click', function () {
 
 //display data on page////
  function displayData(data, word) {
-    console.log(data);
-    infoWord.innerText = `${word}`;
-    phoneticPara.innerText =`${data[0].phonetic}`
-    
-     //play phonetics audio
-    let getPhoneticAudio = data[0]?.phonetics.find((item, index) => {
-        if (item.audio?.length > 0) return true
-      })
-      audio.setAttribute('src', getPhoneticAudio.audio)
-      function playAudio() {
-        audio.play()
-      }
-      audioBtn.addEventListener('click', playAudio)
-    
-      
-    
-     //display meanings
-    const meanings = data[0].meanings[0].definitions;
-    let meaningList = ``;
-    meanings.forEach((meaning, ind) => {
-      meaningList += ` ${ind + 1}. ${meaning.definition}  \n  sentence: ${meaning.example} \n synonyms: ${meaning.synonym } \n`;
-      
-      definition.innerText = meaningList
-      });
+     if ( data.result) {  // if api can't find word
+         error.classList.remove('hidden')
+     } else {
+        error.classList.add('hidden')
+         console.log(data);
+         infoWord.innerText = `${word}`;
+         phoneticPara.innerText =`${data[0].phonetic}`
+         
+          //play phonetics audio
+         let getPhoneticAudio = data[0].phonetics.find((item, index) => {
+             if (item.audio.length > 0) return true
+           })
+           audio.setAttribute('src', getPhoneticAudio.audio)
+           function playAudio() {
+             audio.play()
+           }
+           audioBtn.addEventListener('click', playAudio)
+         
+          //display meanings
+          const meanings = data[0].meanings;
+          let meaningList = ''
+           meanings.map((item)=> {
+               meaningList += displayMeaings(item)
+           })
+           definition.innerHTML = meaningList
+        }
 
-    // //display examples
-    // let exampleList = ``;
-    // meanings.forEach((meaning) => {
-    //     exampleList += `${meaning.example} \n`;
-    //     example.innerText = exampleList
-    //     });
- } 
+        function displayMeaings(meaning) {
+            let otherDisplay = ''
+            meaning.definitions.map((item) => (otherDisplay += otherDisplayFunc(item)))
+            let display = `<div class="block">
+            <header class="text-xl md:text-2xl font-bold capitalize">${meaning.partOfSpeech}</header>
+            <ul class=" list-decimal text-sm md:text-base px-5">
+            ${otherDisplay}
+            </ul>
+            </div>`
 
+            display = display.trim()
+            return display
+        }
+        function otherDisplayFunc(meaning) {
+            definitionsHtml = `<li  class="leading-5 p-2"> ${meaning.definition}<ul>`
+            let exampleList = ``
+            let antonymList = ``
+            let synonymList = ``
 
+            if (meaning.example != undefined) {
+                exampleList += `<li class="italic text-grey font-normal text-muted">sentence: “${meaning.example}"</li>`
+              }
+              if (meaning.synonyms != undefined) {
+                synonymList += `<li class=" font-medium">Synonyms: “${flatArray(
+                  meaning.synonyms,
+                )}”</li>`
+              }
+            
+              if (meaning.antonyms != undefined) {
+                antonymList += `<li class="font-medium">Antonyms: “${flatArray(
+                  meaning.antonyms,
+                )}”</li>`
+              }
+            
+              definitionsHtml += exampleList + synonymList + antonymList + '</ul></li>'
+              return definitionsHtml
+        }
+        function flatArray(arr) {
+            if (arr != undefined) {
+              return arr.reduce((pv, cv) => {
+                return pv + ', ' + cv
+              })
+            }
+        
+ }}
+    
+       
 
 
 
@@ -88,4 +129,8 @@ toggleBtn.addEventListener('click', function () {
     });
    
     searchBtn.addEventListener('click', fetchApi());
+ 
+  
 
+
+        

15b9adada8d3b4e85cda840fe99d02259a33d257
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-03-02 21:19:02 +0100

    ft:implement API consumption

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ var dataDisplay = document.getElementById("data_display")
 var phoneticPara = document.getElementById("phoneticPara")
 var toggleBtn = document.getElementById("toggleBtn")
 var body = document.getElementById("body")
-var error = document.getElementById("error")
-var audio = document.getElementById("audio")
+var errorPage = document.getElementById("error")
+var audioIcon = document.getElementById("audio")
 var audioBtn = document.getElementById("audioBtn")
 var phoneticPara = document.getElementById("phoneticPara")
 
@@ -22,17 +22,18 @@ var phoneticPara = document.getElementById("phoneticPara")
 async function fetchApi(word) {
     infoSkeleton.classList.remove('hidden')
     dataDisplay.classList.add('hidden')
-    error.classList.add('hidden')
+    errorPage.classList.add('hidden')
    try {
        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`; 
        const res = await fetch(url);
        const data = await res.json();
        infoSkeleton.classList.add('hidden')
-       data[0].word === undefined ? dataDisplay.classList.add('hidden') : dataDisplay.classList.remove('hidden')     
-           displayData(data, word);
-
+        dataDisplay.classList.remove('hidden')     
+        word !== undefined ? displayData(data, word) : dataDisplay.classList.add('hidden')
    } catch (error) {
        console.log(error);
+      
+      
    }}
 
 // Toggle light and dark
@@ -42,9 +43,10 @@ toggleBtn.addEventListener('click', function () {
 
 //display data on page////
  function displayData(data, word) {
-     if ( data.result) {  // if api can't find word
+     if ( data.title === 'No Definitions Found' ) {  // if api can't find word
          error.classList.remove('hidden')
-     } else {
+         dataDisplay.classList.add('hidden') 
+     } else  {
         error.classList.add('hidden')
          console.log(data);
          infoWord.innerText = `${word}`;
@@ -54,12 +56,14 @@ toggleBtn.addEventListener('click', function () {
          let getPhoneticAudio = data[0].phonetics.find((item, index) => {
              if (item.audio.length > 0) return true
            })
-           audio.setAttribute('src', getPhoneticAudio.audio)
+           audioIcon.setAttribute('src', getPhoneticAudio.audio)
            function playAudio() {
              audio.play()
            }
            audioBtn.addEventListener('click', playAudio)
          
+          
+   ///////////// Copyright (C) 2022 Bakare Abiola https://github.com/iamstarcode ///////////////
           //display meanings
           const meanings = data[0].meanings;
           let meaningList = ''
@@ -84,51 +88,47 @@ toggleBtn.addEventListener('click', function () {
         }
         function otherDisplayFunc(meaning) {
             definitionsHtml = `<li  class="leading-5 p-2"> ${meaning.definition}<ul>`
-            let exampleList = ``
-            let antonymList = ``
-            let synonymList = ``
+            let exampleList = ''
+            let antonymList = ''
+            let synonymList =''
 
             if (meaning.example != undefined) {
                 exampleList += `<li class="italic text-grey font-normal text-muted">sentence: “${meaning.example}"</li>`
               }
-              if (meaning.synonyms != undefined) {
-                synonymList += `<li class=" font-medium">Synonyms: “${flatArray(
-                  meaning.synonyms,
-                )}”</li>`
+              if (meaning.synonyms != undefined && meaning.synonyms.length > 0) {
+                synonymList += `<li class=" font-medium">Synonyms: “${
+                    flatArray(meaning.synonyms,)}”</li>`
               }
             
-              if (meaning.antonyms != undefined) {
-                antonymList += `<li class="font-medium">Antonyms: “${flatArray(
-                  meaning.antonyms,
-                )}”</li>`
+              if (meaning.antonyms != undefined && meaning.antonyms.length > 0) {
+                antonymList += `<li class="font-medium">Antonyms: “${
+                    flatArray(meaning.antonyms,)}”</li>`
               }
             
               definitionsHtml += exampleList + synonymList + antonymList + '</ul></li>'
               return definitionsHtml
         }
         function flatArray(arr) {
-            if (arr != undefined) {
+            
               return arr.reduce((pv, cv) => {
                 return pv + ', ' + cv
-              })
-            }
-        
+              })    
+        //////////////////////////////////////////////////////////////////////////////////////////
  }}
-    
-       
-
-
 
            // Event Listeners 
     input.addEventListener('keyup', async (e) => {
         
-        if (e.keyCode === 13 && e.target.value) {
-            e.preventDefault();
+        e.preventDefault();
+        if (e.keyCode === 13 && e.target.value.length > 1) {
             await fetchApi(e.target.value);
-        }
+        } 
     });
    
-    searchBtn.addEventListener('click', fetchApi());
+    searchBtn.addEventListener('click', handleSubmit = async (e) => {
+        e.preventDefault();
+        await fetchApi(input.value)
+    });
  
   
 
